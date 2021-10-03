6bdcec1a7b4df82904793520b311b3f2df4edeca
zhcoder <zhcoder@yandex.ru>
2021-10-03 22:51:49 +1000

    add new function upload.single('image')

--- src/routers.js
+++ src/routers.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const api = require('./controllers/api');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const mainRouter = new express.Router();
-mainRouter.post('/upload', api.uploadPicture);
+mainRouter.post('/upload', upload.single('image'), api.uploadPicture);
 mainRouter.get('/list', api.listPictures);
 
 exports.mainRouter = mainRouter;
